7c22b410874c7b66e65c3cb744d98dd8c045c854
Marc <marca0701@gmail.com>
2023-06-02 14:40:30 +0200

    Website laat NFTs zien

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {ConnectWallet, useValidDirectListings, useContract, ThirdwebNftMedia } from "@thirdweb-dev/react";
 import "./styles/Home.css";
 
+const contractAddress = "0x99A50fCc9a5d00409d900892754502Ea1128a0a1"
 export default function Home() {
+  const { contract } = useContract(
+      contractAddress,
+      "marketplace-v3"
+  )
+  const {data: nfts, isLoading} = useValidDirectListings(contract);
+  console.log(nfts, isLoading)
   return (
     <div className="container">
       <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
+        <h1>EduBlocks Catalogue</h1>
+        <ConnectWallet/>
+        {!isLoading ? (
+            <div>
+              {nfts && nfts.map((nft) => {
+                return (
+                    <div key={nft.id}> {nft.asset.name}</div>
+                );
+              })}
+            </div>
+        ) : (
+            <div>Loading...</div>
+        )}
 
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
 
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
-        </div>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
 
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
 
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
+
       </main>
     </div>
   );
